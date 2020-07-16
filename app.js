629662721c1c54a18ac02c497c4729aa5192006c
Irene <airine@Irenes-MacBook-Air.local>
2020-07-16 16:14:57 -0700

    [superHero] add more callback

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function greeting(name, callback) {
-    let sentence = 'Hello, my name is ' + name + '. ';
-    
+    let sentence = 'Hello, my name is ' + name + ' ';
+
     let otherSentence = callback();
 
     return sentence + otherSentence;
@@ -8,56 +8,78 @@ function greeting(name, callback) {
 
 
 function anotherGreeting() {
-    let sentence = 'I forgot, my last name';
+    let sentence = 'I forgot my last name';
+    
     return sentence;
 }
 
 let myName = 'Irene';
 console.log(greeting(myName, anotherGreeting));
 
+// Exercise
 function printArray(array, callback) {
     for (let i = 0; i < array.length; i++) {
-        let number = array[i];
+        let number = array[i]; // 1, 2, ....
 
-        console.log(number);
+        console.log(number); // 1
     }
 
     callback(array);
-
 }
 
 function printArrayAgain(array) {
-    for (i = 0; i < array.length, i++) {
-        let number = array[i];
+    for (let i = 0; i < array.length; i++) {
+        let number = array[i]; // 1, 2 ...
 
-        console.log(number);
+        console.log(number); // 1, 2 
     }
-
 }
 
 const numbersArray = [1,2,3,4,5,9,8,7,6];
 
 printArray(numbersArray, printArrayAgain);
 
-//Exercise
-function calculator (num1, num2, callbackOne, callbackTwo) {
+// Exercise 
+function calculator(num1, num2, callbackOne, callbackTwo) {
     let result = callbackOne(num1, num2) + callbackTwo(num1, num2);
-// 82 + -72 = 10
+    // 82 + -72 = 10
+
     return result;
 }
 
-function addNumbers (num1, num2) {
+function addNumbers(num1, num2) {
     let result = num1 + num2;
-
+    
     return result;
 
 }
 
-function subtractNumber(num1, num2) {
-    let result num1 - num2
-
+function subtractNumbers(num1, num2) {
+    let result = num1 - num2;
+    
     return result;
-
 }
 
-console.log(calculator(5,77, addNumbers, subtractNumber));
+console.log(calculator(5,77, addNumbers, subtractNumbers));
+
+//make a superHero function
+// take in two callbacks
+// return a string in each callback describing the superpower
+
+function superHero(superPowerOne, superPowerTwo) {
+    let allSuperPowers = superPowerOne() + ' and ' + superPowerTwo();
+
+    return allSuperPowers;
+}
+
+function superSkills() {
+    let power = "I'm having fire ball and flash blaze!";
+    return power;
+}
+
+function superSpin() {
+    let spin = 'I can spin 1000 mph';
+    return spin;
+}
+
+console.log(superHero(superSkills, superSpin));
